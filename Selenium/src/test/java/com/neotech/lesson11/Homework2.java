16284863311d6df2cefe54c8aa9002924b36f65c
Ahmet <yasir.aksoy@outlook.com>
2026-01-08 00:16:28 +0300

    lesson11 using common methods homework2

--- Selenium/src/test/java/com/neotech/lesson11/Homework2.java
+++ Selenium/src/test/java/com/neotech/lesson11/Homework2.java
@@ -1,26 +1,25 @@
-package com.neotech.lesson07;
+package com.neotech.lesson11;
 
-import com.neotech.utils.BaseClass;
+import com.neotech.utils.CommonMethods;
 import org.openqa.selenium.By;
 
 import java.util.Iterator;
 import java.util.Set;
 
-public class WindowHandleDemo extends BaseClass {
-
-    // https://accounts.google.com/signup
-    public static void main(String[] args) throws InterruptedException {
-
+public class Homework2 extends CommonMethods {
+    public static void main(String[] args) {
         setUp("https://accounts.google.com/signup");
 
         System.out.println("Title ---> " + driver.getTitle());
 
         System.out.println("Window Handle ---> " + driver.getWindowHandle());
 
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        wait(3);
 
-        driver.findElement(By.linkText("Help")).click();
-
+//        driver.findElement(By.linkText("Help")).click();
+        click(driver.findElement(By.linkText("Help")));
+        
         Set<String> allWindowHandles = driver.getWindowHandles();
         System.out.println(allWindowHandles);
 
@@ -32,23 +31,24 @@ public class WindowHandleDemo extends BaseClass {
         System.out.println("Window 1: " + window1);
         System.out.println("Window 2: " + window2);
 
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        wait(3);
 
         // Just because we have a second window does not mean that the focus is on it.
         // We have to switch into the new window
 
         driver.switchTo().window(window2);
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        wait(3);
 
         String title2 = driver.getTitle();
         System.out.println("Title 2 : " + title2);
 
         // how do i close the second window?
         driver.close();
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
+        wait(3);
 
         tearDown();
-
     }
-
 }
