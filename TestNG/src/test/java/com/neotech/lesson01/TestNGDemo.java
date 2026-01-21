0ab3da4a30b8e73aca4bad4cf2b8bbd6594f83db
Ahmet <yasir.aksoy@outlook.com>
2026-01-21 04:22:54 +0300

    First test with testNG

---
+++ TestNG/src/test/java/com/neotech/lesson01/TestNGDemo.java
@@ -0,0 +1,21 @@
+package com.neotech.lesson01;
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+    @Test
+    public void testOne() {
+        System.out.println("This is the first test method!");
+    }
+
+    @Test
+    public void testTwo() {
+        System.out.println("This is the second test method!");
+    }
+
+
+    @Test
+    public void testThree() {
+        System.out.println("this is third test method!");
+    }
+}
